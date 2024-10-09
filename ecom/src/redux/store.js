27daf6e3976dfb5726a27cec67d1910b6d9133c7
aakashindoriya <aakashindoriya12346@gmail.com>
2024-10-09 08:26:58 +0530

    conflict resolved

--- ecom/src/redux/store.js
+++ ecom/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "./slices/productSlice";
 import blogSlice from "./slices/blogSlice";
+import cartSlice from "./slices/cartSlice";
 //code for redux
 export default configureStore({
   reducer: {
     product: productSlice,
     blog: blogSlice,
+    cart:cartSlice
   },
 });
