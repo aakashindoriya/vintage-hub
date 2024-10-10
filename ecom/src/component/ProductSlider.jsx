eac8009f60d1a0fd7cc82148081fc655810301c2
akanksha_18 <akankshadubey2558@gmail.com>
2024-10-10 10:28:24 +0530

    Slider Update

--- ecom/src/component/ProductSlider.jsx
+++ ecom/src/component/ProductSlider.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import Slider from 'react-slick'; 
-import { Box } from "@chakra-ui/react";
+import { Box, IconButton } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import 'slick-carousel/slick/slick.css'; 
 import 'slick-carousel/slick/slick-theme.css'; 
+
+const CustomPrevArrow = (props) => (
+    <IconButton 
+        onClick={props.onClick} 
+        aria-label="Previous" 
+        icon={<ChevronLeftIcon />} 
+        variant="outline" 
+        position="absolute" 
+        left="10px" 
+        top="50%" 
+        transform="translateY(-50%)"
+        zIndex="1"
+    />
+);
+
+const CustomNextArrow = (props) => (
+    <IconButton 
+        onClick={props.onClick} 
+        aria-label="Next" 
+        icon={<ChevronRightIcon />} 
+        variant="outline" 
+        position="absolute" 
+        right="10px" 
+        top="50%" 
+        transform="translateY(-50%)"
+        zIndex="1"
+    />
+);
 
 const ProductSlider = ({ i }) => {
     const settings = {
@@ -13,6 +42,8 @@ const ProductSlider = ({ i }) => {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000, 
+        prevArrow: <CustomPrevArrow />,
+        nextArrow: <CustomNextArrow />,
     };
 
     return (
