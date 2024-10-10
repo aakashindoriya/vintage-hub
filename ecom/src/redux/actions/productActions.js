3767d5138e8771fbcae31ea9ec3c2e41525f7c45
aakashindoriya <aakashindoriya12346@gmail.com>
2024-10-10 15:30:40 +0530

    product filter is ready

--- ecom/src/redux/actions/productActions.js
+++ ecom/src/redux/actions/productActions.js
@@ -2,12 +2,32 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 
-export const getProducts=createAsyncThunk("product/getproducts",async(limit="")=>{
+export const getProducts=createAsyncThunk("product/getproducts",async()=>{
     try {
-        const {data}= await axios.get(`https://fakestoreapi.com/products?limit=${limit}`)
+        const {data}= await axios.get(`${import.meta.env.VITE_FAKE_STORE_API}/api/product`)
         return data
     } catch (error) {
         console.log(error)
     }
 })
 
+
+export const filterData=(orignalData,category,rating,pricerange)=>{
+    console.log(orignalData,category,rating,pricerange,"in action")
+    let result=[]
+    if(category&&rating){
+        result=orignalData.filter((el)=>el.category===category&&el.totalRating>=rating&&el.price>=pricerange.min&&el.price<=pricerange.max)
+        console.log("first")
+    }
+    else if(category){
+        console.log("second")
+        result=orignalData.filter((el)=>el.category===category&&el.price>=pricerange.min&&el.price<=pricerange.max)
+    }
+    else if(rating){
+        
+        result=orignalData.filter((el)=>el.totalRating>=rating&&el.price>=pricerange.min&&el.price<=pricerange.max)
+    }else{
+        result=orignalData.filter((el)=>el.price>=pricerange.min&&el.price<=pricerange.max)
+    }
+    return result
+}
